ba1264d0460f2fcf27e67fc2ea05a8e21cfcfecf
Javran Cheng <Javran.c@gmail.com>
2015-04-03 20:13:18 -0400

    load edges

--- src/main/scala/com/example/foo/Entry.scala
+++ src/main/scala/com/example/foo/Entry.scala
@@ -1,8 +1,14 @@
 package com.example.foo
 
 import org.apache.spark._
+import org.apache.spark.rdd._
 
 object Entry {
+  type NodeId = Int
+  // TODO: it's implicit that ego connects to every other nodes
+  // so a direct query might not yield desired result.
+  type Edge = (NodeId, NodeId)
+
   def main(args : Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[4]")
     val sc = new SparkContext(conf)
@@ -11,4 +17,11 @@ object Entry {
 	println(textFile.count())
   }
 
+  def loadEdges(sc: SparkContext, fileName : String) : List[Edge] =
+  {
+    val tf = sc.textFile(fileName)
+    val toPair : (Seq[String] => Edge) = {case (a::b::_) => (a.toInt,b.toInt) }
+    val xs : RDD[Edge] = tf.map( line => toPair(line.split("\\s+").toSeq) )
+    return xs.collect().toList
+  }
 }
