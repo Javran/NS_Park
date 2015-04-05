63ecce20c4150f964adfa3f67e9748e3934f7e84
Javran Cheng <Javran.c@gmail.com>
2015-04-04 23:21:31 -0400

    load data

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "1.0"
 scalaVersion := "2.10.4"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.2.1"
+
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.1.1"
--- src/main/scala/com/example/foo/Entry.scala
+++ src/main/scala/com/example/foo/Entry.scala
@@ -8,20 +8,67 @@ object Entry {
   // TODO: it's implicit that ego connects to every other nodes
   // so a direct query might not yield desired result.
   type Edge = (NodeId, NodeId)
+  type Feature = Int
+  type FeatTable = Map[NodeId,Array[Feature]]
+  type CircleTable = Map[String,Set[NodeId]]
+  type FeatNames = Map[Int,Array[String] /* feat desc */ ] // indexed by feature id
 
   def main(args : Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[4]")
     val sc = new SparkContext(conf)
-	val textFile = sc.textFile("data/facebook/0.edges")
-
-	println(textFile.count())
+	loadEdges(sc,"data/facebook/0.edges").foreach(println)
+	loadCircles(sc,"data/facebook/0.circles").foreach(println)
+	println(loadEgoFeat(sc,"data/facebook/0.egofeat").length)
+	loadFeats(sc,"data/facebook/0.feat").foreach(println)
+	loadFeatNames(sc,"data/facebook/0.featnames").foreach( line => {
+	  println(line._1 + ": " + line._2.mkString(">>"))
+	})
   }
 
-  def loadEdges(sc: SparkContext, fileName : String) : List[Edge] =
-  {
-    val tf = sc.textFile(fileName)
-    val toPair : (Seq[String] => Edge) = {case (a::b::_) => (a.toInt,b.toInt) }
-    val xs : RDD[Edge] = tf.map( line => toPair(line.split("\\s+").toSeq) )
-    return xs.collect().toList
+  def loadEdges(sc : SparkContext, fName : String) : List[Edge] = {
+    val textFile = sc.textFile(fName)
+    def toPair (xs :Seq[String]) : Edge = {
+      (xs(0).toInt,xs(1).toInt)
+    }
+	val xs = textFile.map( line => toPair(splitWords(line)) )
+    xs.collect().toList
   }
+
+  def loadCircles(sc: SparkContext, fName : String) : CircleTable = {
+    val textFile = sc.textFile(fName)
+    def toPair (xs : Seq[String]) : (String, Set[NodeId]) = {
+      (xs.head,xs.tail.map( _.toInt ).toSet)
+    }
+    textFile.map( line => toPair(splitWords(line)) ).collect().toMap
+  }
+
+  def loadEgoFeat(sc: SparkContext, fName : String) : Array[Feature]= {
+    val featLine = sc.textFile(fName).take(1)(0) // we only have one line
+    splitWords(featLine).map(_.toInt).toArray
+  }
+
+  def loadFeats(sc: SparkContext, fName: String) : FeatTable = {
+    val textFile = sc.textFile(fName)
+    def toPair (xs : Seq[String]) : (NodeId, Array[Feature]) = {
+      (xs.head.toInt,xs.tail.map( _.toInt ).toArray)
+    }
+    textFile.map( line => toPair(splitWords(line)) ).collect().toMap
+  }
+
+  def loadFeatNames(sc: SparkContext, fName: String) : FeatNames = {
+    val textFile = sc.textFile(fName)
+    def toPair (xs : String) : (NodeId, Array[String]) = {
+      val sep = xs.indexOf(" ")
+      xs.splitAt(sep) match {
+        case (k,v) => (k.toInt,v.tail.split(";"))
+      }
+    }
+    textFile.map( line => toPair(line)).collect().toMap
+  }
+
+
+  def splitWords(raw : String) : List[String] = {
+    raw.split("\\s+").toList
+  }
+
 }
