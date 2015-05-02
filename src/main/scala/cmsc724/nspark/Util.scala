57acff343434ca01fd971167643f18510b59d519
Javran Cheng <Javran.c@gmail.com>
2015-05-02 18:46:33 -0400

    use RDD to store info

--- src/main/scala/cmsc724/nspark/Util.scala
+++ src/main/scala/cmsc724/nspark/Util.scala
@@ -3,14 +3,21 @@ package cmsc724.nspark
 import java.io.File
 
 object Util {
+  // split a string (separated by spaces)
+  // into a list of strings
   def splitWords(raw : String) : Seq[String] = {
     raw.split("\\s+").toList
   }
 
+  // combine two files paths
   def combinePath(path1: String, path2: String): String = {
     val f1 = new File(path1)
     val f2 = new File(f1,path2)
     f2.getPath
+  }
+
+  def fileBaseName(f : File): String = {
+    f.getName().takeWhile(_ != '.')
   }
 
   def addPair[A,B](p: (A,B), d: Map[A,Set[B]]): Map[A,Set[B]] = {
@@ -23,6 +30,14 @@ object Util {
   def addBiPair[A](p: (A,A), d: Map[A,Set[A]]): Map[A,Set[A]] = {
     p match {
       case (a,b) => addPair((b,a),addPair((a,b),d))
+    }
+  }
+
+  def safeToInt(s: String): Option[Int] = {
+    try {
+      Some(s.toInt)
+    } catch {
+      case e:Exception => None
     }
   }
 }
--- src/main/scala/com/example/foo/Entry.scala
+++ src/main/scala/com/example/foo/Entry.scala
@@ -1,25 +1,89 @@
 package com.example.foo
 
+import java.io.File
 import org.apache.spark._
 import cmsc724.nspark.Util
 import cmsc724.nspark.FacebookFilePath
 import cmsc724.nspark.FacebookGraph
+import cmsc724.nspark.Type._
 import org.apache.spark.rdd._
 
 object Entry {
 
-  def main(args : Array[String]) {
+  type FeatureAttr = (NodeId, Set[String])
+
+  def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[4]")
     val sc = new SparkContext(conf)
+    val paths = new FacebookFilePath("data/facebook", 0)
 
-    val paths = new FacebookFilePath("data/facebook",0)
-    val g = new FacebookGraph(sc,paths)
-    // g.circles.foreach(println)
-    val nodes = g.pickNodes( (_,d) => d(Array("work","with","id","anonymized feature 205")) match {
-      case None => false
-      case Some(v) => v == 0
+    // scan the directory to get all ego node names
+    val allEgos: Set[NodeId] = new File(paths.basePath)
+      .listFiles
+      .flatMap(f => Util.safeToInt(Util.fileBaseName(f)))
+      .toSet
+
+    val allEgoFeaturesDict: Map[NodeId, RDD[FeatureAttr]] = allEgos
+      .map(n => {
+        val ps = new FacebookFilePath("data/facebook", n)
+        val fAttr = loadFeatNameFile(ps.featNames, sc)
+        val egoFeature = (ps.egoId, loadEgoFeature(ps.egoFeat, sc, fAttr))
+        val allFeats = sc.parallelize(Seq(egoFeature)) ++ loadFeature(ps.feat, sc, fAttr)
+        (n, allFeats)
+      }).toMap
+
+    val allEgoEdges: Map[NodeId, RDD[(NodeId,NodeId)]] = allEgos
+      .map( n => {
+        val ps = new FacebookFilePath("data/facebook", n)
+        val edges = loadEdges(ps.edges,sc)
+        (n,edges)
+      }).toMap
+  }
+
+  def loadEdges(path: String, sc: SparkContext): RDD[(NodeId,NodeId)] = {
+    sc.textFile(path).map(raw => {
+      val splitted = Util.splitWords(raw).map(_.toInt)
+      assert(splitted.length == 2)
+      (splitted(0),splitted(1))
+    })
+  }
+
+  def loadEgoFeature(path: String, sc: SparkContext, featNameArr: Array[String]): Set[String] = {
+    val raw: RDD[String] = sc.textFile(path)
+    assert(raw.count() == 1);
+    val splitted = Util.splitWords(raw.first).map(_.toInt)
+    assert(splitted.length == featNameArr.length)
+    val attrs: Set[String] = (featNameArr zip splitted)
+      .flatMap { case (a, i) => if (i == 1) Some(a) else None }
+      .toSet
+    attrs
+  }
+
+  def loadFeature(path: String, sc: SparkContext, featNameArr: Array[String]): RDD[FeatureAttr] = {
+    sc.textFile(path).map(l => {
+      val cols = Util.splitWords(l).map(_.toInt)
+      assert(cols.tail.length == featNameArr.length)
+      val attrs = (featNameArr zip cols.tail)
+        .flatMap(p =>
+          if (p._2 == 1) Some(p._1)
+          else None)
+        .toSet
+      (cols.head, attrs)
+    })
+  }
+
+  // load feature name files
+  def loadFeatNameFile(path: String, sc: SparkContext): Array[String] = {
+    def splitAtFirstSpace(xs: String): (Long, String) = xs.span(_ != ' ') match {
+      case (l, c) => (l.toLong, c)
     }
-    )
-    nodes.foreach(println)
+
+    def splitAndVerify(raw: String, n: Long): String = splitAtFirstSpace(raw) match {
+      case (l, c) => assert(n == l); c
+    }
+    sc.textFile(path)
+      .zipWithIndex
+      .map(d => d match { case (n, l) => splitAndVerify(n, l) })
+      .toArray
   }
 }
