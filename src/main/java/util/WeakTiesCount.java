598fdf61669f11dc3673203bc03abc08c177351d
Doowon Kim <gotapenny@gmail.com>
2015-05-06 12:08:10 -0700

    divided by 2

--- src/main/java/util/WeakTiesCount.java
+++ src/main/java/util/WeakTiesCount.java
@@ -9,13 +9,14 @@ import java.util.Set;
 
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Vertex;
 
 public class WeakTiesCount {
 	public static Map<Object,Set<Object>> buildAdjacencyMap(List<Edge> edges){
 		if ((edges==null) || (edges.isEmpty())){
 			return Collections.<Object,Set<Object>>emptyMap();
 		}
-		
+
 		Map<Object,Set<Object>> graph = new HashMap<>();
 		for (Edge e : edges){
 			if (!graph.containsKey(e.getVertex(Direction.IN))){
@@ -27,25 +28,28 @@ public class WeakTiesCount {
 			graph.get(e.getVertex(Direction.IN)).add(e.getVertex(Direction.OUT));
 			graph.get(e.getVertex(Direction.OUT)).add(e.getVertex(Direction.IN));
 		}
-		
+
 		return graph;
 	}
-	
-	public static int counts(List<Edge> edges){
+
+	public static int counts(Vertex center, List<Edge> edges){
 
 		Map<Object,Set<Object>> graph = buildAdjacencyMap(edges);
-
+		
 		int weakTies = 0;
-		for (Set<Object> neighbors : graph.values()){
-			for (Object v2 : neighbors){
-				for (Object v3 : neighbors){
-					if ((!v2.equals(v3)) && !(graph.get(v2).contains(v3))){
-						weakTies++;
-					}
+		//		for (Set<Object> neighbors : graph.values()){
+		Set<Object> neighbors = graph.get(center);
+		for (Object v2 : neighbors){
+			for (Object v3 : neighbors){
+				if ((!v2.equals(v3)) && !(graph.get(v2).contains(v3))){
+					weakTies++;
 				}
 			}
+			//			}
 		}
-		System.out.println(weakTies/6);
-		return (weakTies/6);
+
+
+		System.out.println(weakTies/2);
+		return (weakTies/2);
 	}
 }
